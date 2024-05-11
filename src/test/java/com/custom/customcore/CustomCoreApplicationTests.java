b60e7e952fa81e6e66ffe8d7163938e15e4a1f42
Rohit-505 <vrohit569@gmail.com>
2024-05-12 00:58:57 +0530

    Initial Commit

---
+++ src/test/java/com/custom/customcore/CustomCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.custom.customcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
